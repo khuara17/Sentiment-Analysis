710436266af47024040f1716860257c8fb966342
Vishal Kamlapure <33551500+khuara17@users.noreply.github.com>
2022-07-01 16:54:54 +0530

    Security update

--- acomparativesanalysis/settings.py
+++ acomparativesanalysis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q!7)zly@^&x!jk%+zs!&y&s9dv#kf)i*nwe^!0bwo6tsx3bl3i'
+SECRET_KEY = '****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
